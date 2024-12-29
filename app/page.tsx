eda72a3979076488c1e2af006882f0fe038e0fe4
rafael <rafaeld.geraldo2@gmail.com>
2024-12-29 01:00:09 -0500

    Implement draggable items

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
 import CategoryColumn from "@/components/column";
-import MediaCard from "@/components/rating-card";
-import { DndContext, DragOverlay } from "@dnd-kit/core";
+import MediaCard, { StaticMediaCard } from "@/components/rating-card";
+import {  DndContext, DragOverlay, MeasuringStrategy } from "@dnd-kit/core";
+import { DropAnimationSideEffects, KeyframeResolver } from "@dnd-kit/core/dist/components/DragOverlay/hooks/useDropAnimation";
+import { CSS } from '@dnd-kit/utilities';
+
 import { useState } from "react";
 
-
 export default function Home() {
+  
+  const [filter, setFilter] = useState(CategoryFilter.All)
 
   const [unstarted, setUnstarted] = useState([
     { title: "Bleach", rating: undefined },
@@ -15,17 +19,25 @@ export default function Home() {
 
   const [ongoing, setOngoing] = useState([
     { title: "Naruto", rating: 8.5 },
+    { title: "Re:Zero", rating: 9.5 },
     { title: "That Time I Got Reincarnated as a Slime", rating: 8 }
   ])
 
   const [finished, setFinished] = useState([
     { title: "Attack on Titan", rating: 10 },
     { title: "Cyberpunk Edgerunners", rating: 10 },
+    { title: "Vinland Sage", rating: 10 },
+    { title: "One Punch Man", rating: 7 },
+    { title: "JJK", rating: 8.5 },
+    { title: "Dandadan", rating: 8.5 },
+    { title: "Kaiju No. 8", rating: 8.5 },
+    { title: "Chainsaw Man", rating: 9 },
     { title: "Frieren", rating: 7.5 },
   ])
 
   const [dropped, setDropped] = useState([
-    { title: "Tower of God", rating: 7 }
+    { title: "Tower of God", rating: 7 },
+    { title: "Made in Abyss", rating: 8 }
   ])
 
 
@@ -54,14 +66,95 @@ export default function Home() {
   }
 
 
+  const [activeMedia, setActiveMedia] = useState<{title: string, rating: number | undefined} | null>(null);
+
+  const measuringConfig = {
+    droppable: {
+      strategy: MeasuringStrategy.Always,
+    }
+  };
+
+  const defaultKeyframeResolver: KeyframeResolver = ({
+    transform: { initial, final },
+    
+  }) => { 
+    return [
+      {
+        transform: CSS.Transform.toString(initial),
+      },
+      {
+        transform: (CSS.Transform).toString(final),
+      },
+    ]
+  };
+
+  const defaultDropAnimationSideEffects = (
+    options: { styles: any; className: any; }
+  ): DropAnimationSideEffects => ({ active, dragOverlay }) => {
+    const originalStyles: Record<string, string> = {};
+    const { styles, className } = options;
+
+    if (styles?.active) {
+      for (const [key, value] of Object.entries(styles.active)) {
+        if (value === undefined) {
+          continue;
+        }
+
+        originalStyles[key] = active.node.style.getPropertyValue(key);
+        active.node.style.setProperty(key, value as string);
+      }
+    }
+
+    if (styles?.dragOverlay) {
+      for (const [key, value] of Object.entries(styles.dragOverlay)) {
+        if (value === undefined) {
+          continue;
+        }
+
+        dragOverlay.node.style.setProperty(key, value as string);
+      }
+    }
+
+    console.log(className?.active);
+    
+    if (className?.active) {
+      active.node.classList.add(className.active);
+    }
+
+    if (className?.dragOverlay) {
+      dragOverlay.node.classList.add(className.dragOverlay);
+    }
+
+    return function cleanup() {
+      for (const [key, value] of Object.entries(originalStyles)) {
+        active.node.style.setProperty(key, value);
+      }
+
+      if (className?.active) {
+        active.node.classList.remove(className.active);
+      }
+    };
+  };
+
 
   return (
     <div className="flex flex-row h-dvh p-8 overflow-hidden">
 
       <DndContext
-
-        onDragEnd={
+        autoScroll={false}
+        measuring={measuringConfig}
+        onDragStart={
           (event) => {
+            console.log(event);
+            
+            const { rating } = (event.active.data.current as { rating: number | undefined })
+            const title = event.active.id
+            setActiveMedia({ title: title as string, rating: rating } )
+          }
+        }
+        onDragEnd={
+          (event) => {            
+            setActiveMedia(null)
             const { category, rating } = (event.active.data.current as { category: string, rating: number | undefined})
             const title = event.active.id
             
@@ -75,11 +168,11 @@ export default function Home() {
             const { media: newMedia, setMedia: setNewMedia } = categories[newCategory]
 
             setOldMedia(oldMedia.filter(media => media.title != title))
-            setNewMedia([...newMedia, { title: title, rating: rating }])
+            // consider small delay here as opposed to animation
+            setTimeout( () => {
+              setNewMedia([...newMedia, { title: title, rating: rating }])
 
-            
-            
-            
+            }, 150)
 
           }
         }
@@ -89,12 +182,38 @@ export default function Home() {
         {
           
           Object.keys(categories).map(category =>
-            <CategoryColumn categoryName={category} key={category} hoverColor={ categories[category].hoverColor }>
+          (filter == CategoryFilter.All || filter == (CategoryFilter as any)[category]) &&
+            <CategoryColumn
+              onFilter={ () => {
+                  const newFilter: CategoryFilter = filter == CategoryFilter.All ? (CategoryFilter as any)[category] : CategoryFilter.All
+                  setFilter(newFilter)
+                  return newFilter
+              }}
+              categoryName={category} key={category} hoverColor={ categories[category].hoverColor 
+            }>
               {categories[category].media.sort((a, b) => a.title.localeCompare(b.title)).map((media) => <MediaCard category={category} title={media.title} rating={media.rating} key={media.title} ></MediaCard>)}
             </CategoryColumn>
           )
         }
-        
+        <DragOverlay 
+          dropAnimation={{ duration: 500, easing: "ease-in-out",
+            keyframes: defaultKeyframeResolver,
+            sideEffects: defaultDropAnimationSideEffects({
+              styles: {
+                active: {
+                  opacity: ".5"
+                },
+              },
+              className: {
+                active: "card"
+              }
+            }),
+          }}
+        >
+          {activeMedia &&<StaticMediaCard category={""} title={activeMedia.title} rating={activeMedia.rating} ></StaticMediaCard>}
+        </DragOverlay>
+
+
       </DndContext>
 
 
@@ -102,7 +221,13 @@ export default function Home() {
   );
 }
 
-
+export enum CategoryFilter {
+  Unstarted,
+  Ongoing,
+  Finished,
+  Dropped,
+  All
+}
 
 interface CategoryMap {
   [key: string]: {
@@ -114,3 +239,4 @@ interface CategoryMap {
     setMedia: any
   }
 }
+
--- components/column.tsx
+++ components/column.tsx
@@ -1,21 +1,41 @@
 "use client";
 
+import { CategoryFilter } from '@/app/page';
 import { useDroppable } from '@dnd-kit/core';
+import { useState } from 'react';
 
 
-export default function CategoryColumn({ children, categoryName, hoverColor }: { children?: React.ReactNode, categoryName: string, hoverColor: string }) {
+export default function CategoryColumn({ children, categoryName, hoverColor, onFilter }: 
+    { children?: React.ReactNode, categoryName: string, hoverColor: string,  onFilter: () => CategoryFilter 
+
+}) {
+
+    const [ columnView, setColumnView ] = useState(true)
+
     const { isOver, setNodeRef } = useDroppable({
         id: categoryName,
     });
+
     const style: React.CSSProperties = {
         backgroundColor: isOver ? hoverColor : "white",
-        transition: "all ease-in-out .5s"
+        transition: "all ease-in-out .5s",
     };
 
     return (
-        <div id={categoryName} ref={setNodeRef} style={style } className="flex flex-col flex-1 p-2">
-            <span className=" border rounded-lg w-fit px-5 hover:cursor-pointer bg-white mb-3">{categoryName}</span>
-            {children}
+        <div id={categoryName} ref={setNodeRef} style={style} className="flex flex-col p-2 w-screen">
+            <span onClick={ () => {
+                setColumnView(onFilter() != CategoryFilter.All) // call filter function and use value to set column view
+            }} 
+            className=" border rounded-lg w-fit px-5 hover:cursor-pointer bg-white mb-3"
+        >{categoryName}</span>
+
+            <div className=' no-scrollbar h-dvh overflow-y-scroll'>
+                <div className='flex flex-col flex-nowrap gap-3'>
+                    {children}
+                </div>
+            </div>
+            
+            
         </div>
     );
 }
