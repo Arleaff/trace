cfd7c1d4a758e33116c994ba7ff4579b17309643
rafael <arleaffdev@gmail.com>
2025-03-06 18:14:46 -0500

    Fix media card

--- components/media-card.tsx
+++ components/media-card.tsx
@@ -2,29 +2,11 @@
 import { useDraggable } from '@dnd-kit/core';
 import MediaDialog from './media-dialog';
 import { Media } from '@/app/home';
-import { Avatar, Progress } from '@radix-ui/themes';
-import { memo } from 'react';
+import { Progress, Theme } from '@radix-ui/themes';
+import { memo, useRef } from 'react';
 import { CSS } from '@dnd-kit/utilities';
-import { Portal } from 'radix-ui';
+import { Portal, Avatar } from 'radix-ui';
 
-
-
-function ProgressCircle({ rating }: { rating: number | null }) {
-    const size = 60
-    const radius = 25
-    const dashArray = radius * 2 * Math.PI
-    const strokeWidth = 8
-
-    return (
-        <svg width={size} height={size} className="flex flex-none">
-            <g transform={`rotate(-90 ${size / 2} ${size / 2})`}>
-                <circle r={radius} cx={size / 2} cy={size / 2} fill="transparent" stroke="lightgrey" strokeWidth={`${strokeWidth}px`} strokeDasharray={dashArray} strokeDashoffset="0"></circle>
-                <circle className="progress" stroke="#75DDDD" strokeDashoffset={rating ? dashArray * (1 - rating / 10) : dashArray} r={radius} cx={size / 2} cy={size / 2} fill="transparent" strokeLinecap="round" strokeWidth={`${strokeWidth}`} strokeDasharray={dashArray} />
-            </g>
-            <text x="50%" y="50%" dominantBaseline="central" textAnchor="middle" className={` font-sans ${rating ? "text-lg" : "text-base"} font-medium content-center`}>{rating || "N/A"}</text>
-        </svg>
-    )
-}
 
 
 
@@ -52,8 +34,17 @@ export const MediaCard = memo(function MediaCard({ media, onEdit = () => { }, on
         position: isDragging ? 'fixed' : "relative",
     };
 
+    // const fallback = getTitleLetters(media.title)
+
+
     const CardChildren = memo(() => {
         return (<>
+            <Avatar.Root className="inline-flex size-[45px] select-none items-center justify-center overflow-hidden rounded-lg  align-middle">
+                <Avatar.Fallback className="leading-1 flex size-full items-center justify-center text-[15px] font-medium bg-[#0058FF20] text-[#002AB3C9]">
+                    {fallback}
+                </Avatar.Fallback>
+            </Avatar.Root>
+
             <div className='ml-4 flex flex-col flex-1'>
                 <span className="line-clamp-2 text-start">{media.title}</span>
                 <div className='flex items-center'>
@@ -70,10 +61,13 @@ export const MediaCard = memo(function MediaCard({ media, onEdit = () => { }, on
     const progressValue = (media.rating ?? 0) * 10
     const fallback = getTitleLetters(media.title)
 
+
     return (
         <>
         {/* Render in portal when dragging */}
         { isDragging ? 
+                <Theme asChild>
+
                     <Portal.Root asChild>
                         <div
                             ref={setNodeRef}
@@ -83,9 +77,12 @@ export const MediaCard = memo(function MediaCard({ media, onEdit = () => { }, on
                             aria-describedby=''
                             className="flex flex-row items-center border-2 rounded-xl pl-3 pr-1 py-2 select-none max-w-sm shadow-sm hover:shadow-md bg-white w-full my-1"
                         >
-                            <CardChildren/>
+                        <CardChildren />
+
                         </div>
                     </Portal.Root>
+                </Theme>
+
         :
                     <div
                         ref={setNodeRef}
