03e5e8c9905fe8ce5ef1a10d4a0715ffe323f72d
rafael <arleaffdev@gmail.com>
2025-03-22 12:19:49 -0400

    Attemp dynamic route

--- auth/oauth.ts
+++ auth/oauth.ts
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const google = new Google(
     process.env.GOOGLE_CLIENT_ID ?? "",
     process.env.GOOGLE_CLIENT_SECRET ?? "",
-    "https://media-tracker-eight.vercel.app/login/google/callback"
+    "https://" + process.env.VERCEL_URL + "/login/google/callback"
 );
