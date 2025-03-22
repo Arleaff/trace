17a8fb08844a7d40a6fd2bc0d238318774206a55
rafael <arleaffdev@gmail.com>
2025-03-22 12:03:42 -0400

    Change callback url to http

--- auth/oauth.ts
+++ auth/oauth.ts
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const google = new Google(
     process.env.GOOGLE_CLIENT_ID ?? "",
     process.env.GOOGLE_CLIENT_SECRET ?? "",
-    "https://" + process.env.VERCEL_URL + "/login/google/callback"
+    "http://" + process.env.VERCEL_URL + "/login/google/callback"
 );
